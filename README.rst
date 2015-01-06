85b2bf55186722a1fca83b7ba7dbffc73144a915
Will Kahn-Greene <willkg@bluesock.org>
2015-01-06 14:54:22 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,18 @@
+======
+README
+======
+
+Summary
+=======
+
+Installs rdalal on your system.
+
+
+Install and configure
+=====================
+
+1. Create a virtual environment.
+
+2. Install the dependencies::
+
+       $ pip install rdalal
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import sys
+
+
+# Installing rdalal will open the video in your browser.
+if 'egg_info' in sys.argv or 'install' in sys.argv:
+    import webbrowser
+    webbrowser.open('https://www.youtube.com/watch?v=NMZcwXh7HDA', new=2, autoraise=True)
+
+
+setup(
+    name='rdalal',
+    version='1.0',
+    description='Install some sweet Rehan',
+    author='Will Kahn-Greene',
+    author_email='willkg@bluesock.org',
+    url='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
